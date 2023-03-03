df54519bf06e40f682fd62667de8b3b9aa0a73db
saulo-silv <vitorgabrielsaulo@gmail.com>
2023-03-03 08:17:44 -0300

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className="footer">
       <a href="https://github.com/saulo-silv/Marvel" target="_blank">
-        Developed by &copy;Saulo__Silva
+        Repositório para mais detalhes!
       </a>
     </div>
   );
