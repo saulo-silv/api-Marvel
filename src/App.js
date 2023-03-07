6cc07d1b277ecc0e870be02d3ce761b2ccf9f252
saulo-silv <vitorgabrielsaulo@gmail.com>
2023-03-07 08:02:56 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import Navbar from "./components/Navbar";
 import NavItem from "./components/Navbar/NavItem";
 import Logo from "./components/Navbar/Logo";
-import About from "./components/About";
+//import About from "./components/About";
 import HeroDetails from "./components/HeroDetails.jsx";
 import Home from "./components/Home";
-
 import Footer from "./components/Footer";
-
 import Loogo from'./components/Images/marvel.jpg';
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
           <img src={Loogo} style={{ width: '100%',  height: '170px'}}/>
-
       <Navbar logo={<Logo text={""} />}>
         <NavItem to={"/"} text={"Home"} />
         <NavItem to={"/about"} text={"About"} />
@@ -28,5 +24,4 @@ function App() {
     </Router>
   );
 }
-
 export default App;
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,18 +1,11 @@
-import MD5 from "crypto-js/md5";
 import axios from "axios";
+import { MD5 } from "crypto-js";
 
-//https://gateway.marvel.com:443/v1/public/characters?apikey=81a7e3759ba5a8d50578600e0aa3cad6
-//key public: 81a7e3759ba5a8d50578600e0aa3cad6
-//key private: b116d0b0d900ee102e62f03d08b199e95d1fa8e2
-//ts: 1
-// 1b116d0b0d900ee102e62f03d08b199e95d1fa8e281a7e3759ba5a8d50578600e0aa3cad6
-//result: 3a99aa23d3c11aba1f76bbfce533affb
+let API_URL = process.env.REACT_APP_BASE_URL;
 
 const getHash = (ts, privateKey, publicKey) => {
   return MD5(ts + privateKey + publicKey).toString();
 };
-  
-let API_URL = process.env.REACT_APP_BASE_URL;
 
 const fetchHeros = async (name) => {
   let heroUrl = `${API_URL}/v1/public/characters`;
@@ -24,12 +17,10 @@ const fetchHeros = async (name) => {
   let url = `${heroUrl}?ts=${ts}&apikey=${apiKey}&hash=${hash}&nameStartsWith=${name}`;
 
   try {
-    let response = await fetch(url);
-    let data = await response.json();
-    console.log(data);
-    return data.data.results;
+    const response = await axios.get(url);
+    return response.data.data.results;
   } catch (err) {
-    console(err);
+    console.log(err);
     return;
   }
 };
@@ -44,8 +35,8 @@ const fetchHero = async (id) => {
   let url = `${heroUrl}?ts=${ts}&apikey=${apiKey}&hash=${hash}`;
 
   try {
-    const result = await axios({method: "GET",url});
-    return result.data.data.results
+    const response = await axios.get(url);
+    return response.data.data.results;
   } catch (err) {
     console.error(err);
     return err;
