785713155ea3ceec2e1090f593389c3ab516a2cf
saulo-silv <vitorgabrielsaulo@gmail.com>
2023-03-08 10:14:33 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Navbar from "./components/Navbar";
 import NavItem from "./components/Navbar/NavItem";
 import Logo from "./components/Navbar/Logo";
-//import About from "./components/About";
+import About from "./components/About";
 import HeroDetails from "./components/HeroDetails.jsx";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 import Loogo from'./components/Images/marvel.jpg';
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
       </Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="/:id" element={<HeroDetails />} />
       </Routes>
       < Footer />
---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <h3>
+      A Marvel Comics é uma das maiores editoras de histórias em quadrinhos do
+      mundo, famosa por criar personagens icônicos como Homem-Aranha, X-Men,
+      Capitão América, Homem de Ferro, Thor, Hulk, entre outros. Com mais de 80
+      anos de história, a Marvel Comics é conhecida por suas tramas
+      emocionantes, personagens complexos e uma abordagem realista dos problemas
+      enfrentados pelos super-heróis em suas jornadas. Além dos quadrinhos, a
+      Marvel expandiu seu universo para filmes, séries, jogos e outros produtos,
+      conquistando fãs de todas as idades em todo o mundo.
+    </h3>
+  );
+}
